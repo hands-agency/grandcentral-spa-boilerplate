2390bb2c2be6a4e88b4eba36f2221f756fa9b94a
victor-gabou <victor.gabou@outlook.fr>
2017-09-18 14:04:40 +0200

    external link open in target blank

--- biggie/assets/js/utils/biggie/route.js
+++ biggie/assets/js/utils/biggie/route.js
@@ -5,8 +5,12 @@ export default (e) => {
 
     const target = e.currentTarget
 
+    const url = target.href
+    if (!url.match(window.location.origin)) {
+      target.setAttribute('target', '_blank');
+    }
     if(classes.has(target, 'no-route') || (target.hasAttribute('target') && target.getAttribute('target') == '_blank')) return
-    
+
     e.preventDefault()
 
     framework.go(target.getAttribute('href'))
